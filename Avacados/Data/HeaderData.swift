0c518d06f781da4c4b10d8ac8347dbec9f99db01
Gan Tu <github@tugan.dev>
2023-09-04 12:57:39 -0400

    feat: add headers data

---
+++ Avacados/Data/HeaderData.swift
@@ -0,0 +1,16 @@
+//
+//  HeaderData.swift
+//  Avacados
+//
+//  Created by Gan Tu on 9/4/23.
+//
+
+import SwiftUI
+
+let headersData: [Header] = [
+    Header(
+        image: "avocado-slice-1",
+        headline: "Avocados",
+        subheadline: "Avocados are a powerhouse ingredient at any meal for anyone"
+    )
+]
---
+++ Avacados/Modal/HeaderModel.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderModel.swift
+//  Avacados
+//
+//  Created by Gan Tu on 9/4/23.
+//
+
+import SwiftUI
+
+struct Header: Identifiable {
+    var id = UUID()
+    var image: String
+    var headline: String
+    var subheadline: String
+}
