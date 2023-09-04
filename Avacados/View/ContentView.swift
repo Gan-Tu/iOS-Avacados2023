92a0b6f9eb2b9ef13d3816609b44a9015b9de99b
Gan Tu <github@tugan.dev>
2023-09-04 12:53:56 -0400

    feat: add initial recipes view

--- Avacados/View/ContentView.swift
+++ Avacados/View/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
+                // MARK: - HEADER
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top,  spacing: 0) {
+                        HeaderView()
+                    }
+                }
+                
+                // MARK: - FOOTER
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundStyle(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    
+                    Text("Everything you wanted to know about avocados but were too afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(Color.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
