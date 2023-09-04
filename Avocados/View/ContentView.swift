ce2814ae9d5363cb8d8c99db464dd49b33482709
Gan Tu <github@tugan.dev>
2023-09-04 13:24:36 -0400

    feat: add dishes view

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -23,18 +23,25 @@ struct ContentView: View {
                     }
                 }
                 
+                // MARK: - DISHES
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+
+                DishesView()
+                    .frame(maxWidth: 640)
+                
                 // MARK: - FOOTER
                 VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
                     Text("All About Avocados")
-                        .font(.system(.title, design: .serif))
                         .fontWeight(.bold)
-                        .foregroundStyle(Color("ColorGreenAdaptive"))
-                        .padding(8)
+                        .modifier(TitleModifier())
                     
                     Text("Everything you wanted to know about avocados but were too afraid to ask.")
                         .font(.system(.body, design: .serif))
                         .multilineTextAlignment(.center)
                         .foregroundStyle(Color.gray)
+                        .frame(minHeight: 60)
                 }
                 .frame(maxWidth: 640)
                 .padding()
@@ -43,6 +50,15 @@ struct ContentView: View {
         }
         .edgesIgnoringSafeArea(.all)
         .padding(0)
+    }
+}
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.title, design: .serif))
+            .foregroundStyle(Color("ColorGreenAdaptive"))
+            .padding(8)
     }
 }
 
