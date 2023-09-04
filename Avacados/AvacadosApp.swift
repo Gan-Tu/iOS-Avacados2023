0210be45f8c58d53262a8ddc528cd86876e62e87
Gan Tu <github@tugan.dev>
2023-09-04 11:50:50 -0400

    Initial Commit

---
+++ Avacados/AvacadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvacadosApp.swift
+//  Avacados
+//
+//  Created by Gan Tu on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AvacadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avacados/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Avacados
+//
+//  Created by Gan Tu on 9/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
